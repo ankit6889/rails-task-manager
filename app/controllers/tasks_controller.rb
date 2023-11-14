220f10717dbcf3d7bbcb46a2b40294a3d50ac293
Ankit Kumar <ankit@gypsyphi.com>
2023-11-14 14:29:49 +0800

    Challange - RailsTask Manager, completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_path(@tasks)
+  end
+
+  def edit
+    set_params
+  end
+
+  def update
+    set_params
+    @task.update(task_params)
+    redirect_to tasks_path(@tasks)
+  end
+
+  def destroy
+    set_params
+    @task.destroy
+    # No need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path(@tasks), status: :see_other
+  end
+
+  private
+
+  def set_params
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_14_032242) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
